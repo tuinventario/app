5ca305593e811966eace68887bdfe721e2e1131d
MaryChindoy <uammarychindoy@gmail.com>
2022-10-25 08:15:32 -0500

    ajuste visual de la vista ventas

--- src/components/Sales.js
+++ src/components/Sales.js
@@ -13,31 +13,31 @@ function Sales(){
                             <table class="table table-bordered table-hover">
                                 <thead class="table-warning table">
                                     <tr>
-                                        <th scope="col" >Fecha de venta</th>
-                                        <th scope="col" >Id. Venta</th>
-                                        <th scope="col">Valor</th>  
+                                        <th scope="col"  className="centered">Fecha de venta</th>
+                                        <th scope="col" className="centered">Id. Venta</th>
+                                        <th scope="col" className="centered">Valor</th>  
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <tr>
-                                        <td className="centered">06/10/2022</td>
-                                        <td className="centered">1203</td>
-                                        <td className="centered">$4.000</td>
+                                        <td className="centered text-white">06/10/2022</td>
+                                        <td className="centered text-white">1203</td>
+                                        <td className="centered text-white">$4.000</td>
                                     </tr>
                                     <tr>
-                                        <td className="centered">08/10/2022</td>
-                                        <td className="centered">1423</td>
-                                        <td className="centered">$6.000</td>
+                                        <td className="centered text-white">08/10/2022</td>
+                                        <td className="centered text-white">1423</td>
+                                        <td className="centered text-white">$6.000</td>
                                     </tr>
                                     <tr>
-                                        <td className="centered">10/10/2022</td>
-                                        <td className="centered"> 1263</td>
-                                        <td className="centered">$10.000</td>
+                                        <td className="centered text-white">10/10/2022</td>
+                                        <td className="centered text-white"> 1263</td>
+                                        <td className="centered text-white">$10.000</td>
                                     </tr>
                                     
                                     <tr>
-                                        <th colspan="2" className="centered">Total</th>
-                                        <td className="centered">$20.000</td>
+                                        <th colspan="2" className="centered text-white">Total</th>
+                                        <td className="centered text-white">$20.000</td>
                                     </tr>
                                                             
                                 </tbody>
