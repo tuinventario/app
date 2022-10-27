fbc4533546f9110a512c4fcf483f611c0360e0a6
eapabon3245 <eapabon32@gmail.com>
2022-10-27 05:57:09 -0500

    merge con main

--- src/components/Shopping.js
+++ src/components/Shopping.js
@@ -1,89 +1,102 @@
+import React, { useState  } from "react";
+import Message from "./Message";
 function Shopping() {
+    var [show, setShow] =  useState(false);
+    var children="Se ha agregado correctamente";
+    var showModal = () => {
+        setShow(true);
+    
+    }
+    
+    var hideModal = () => {
+        setShow(false);
+    }
     return (
         <div class="container-fluid p-0 mb-5" >
-        <div class="text-center hero my-5 p position-relative" >
-          <img class="d-block w-100" src="../img/papeleria.png" alt=""/>
-          <div class="carousel-caption">
-            <div class="row col-12 h-100">
+        <Message show={show} handleClose={hideModal} children={children}/>
+       <div class="text-center hero my-5 p position-relative" >
+         <img class="d-block w-100" src="../img/papeleria.png" alt=""/>
+         <div class="carousel-caption">
+           <div class="row col-12 h-100">
 
-            <div class="my-2 mx-2 d-flex gap-3">
-                <div class="col-8 d-flex flex-column justify-content-start">
-                    <div class="p-2 d-flex gap-4">
-                        <div> 
-                            <img src="../img/882470265.jpg"  width="100rem" height="auto gap-2" alt="..."/>
-                        </div>
-                        <div>
-                            <h2 class="ml-1">Mochila KL</h2>
-                            <h6 class="text-muted">SKU 882470265</h6>
-                            <h6 class="text-muted">Negro</h6>
-                            <button type="button" class="btn btn-dark">Eliminar</button>
-                        </div>
+           <div class="my-2 mx-2 d-flex gap-3">
+               <div class="col-9">
+                   <table class="table  text-white">
+                       <tbody>
+                       <tr>
+                       <td> 
+                           <img src="../img/882470265.jpg"  width="100rem" height="auto gap-2" alt="..."/>
+                       </td>
+                       <td class="align-middle">
+                           <h6 class="ml-1">Mochila KL</h6>
+                       </td>
 
-                        <div>
-                            <h2>$119.900</h2>
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <label class="input-group-text" for="inputGroupSelect01">Cantidad</label>
-                                </div>
-                                <select class="custom-select" id="inputGroupSelect01">
+                       <td class="align-middle">
+                           <h6>$119.900</h6>
+                       </td>
+                       <td class="align-middle">
+                           <h6>Cantidad: 4</h6>
+                       </td>
+                       <td class="align-middle">
+                           <button type="button" class="btn btn-dark">Eliminar</button>
+                       </td>
+                       </tr>
+                       <tr>
+                       <td> 
+                           <img src="../img/882470265.jpg"  width="100rem" height="auto gap-2" alt="..."/>
+                       </td>
+                       <td class="align-middle">
+                           <h6 class="ml-1">Mochila KL</h6>
+                       </td>
 
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
+                       <td class="align-middle">
+                           <h6>$119.900</h6>
+                       </td>
+                       <td class="align-middle">
+                           <h6>Cantidad: 4</h6>
+                       </td>
+                       <td class="align-middle">
+                           <button type="button" class="btn btn-dark">Eliminar</button>
+                       </td>
+                       </tr>
+                       <tr>
+                       <td> 
+                           <img src="../img/882470265.jpg"  width="100rem" height="auto gap-2" alt="..."/>
+                       </td>
+                       <td class="align-middle">
+                           <h6 class="ml-1">Mochila KL</h6>
+                       </td>
 
+                       <td class="align-middle">
+                           <h6>$119.900</h6>
+                       </td>
+                       <td class="align-middle">
+                           <h6>Cantidad: 4</h6>
+                       </td>
+                       <td class="align-middle">
+                           <button type="button" class="btn btn-dark">Eliminar</button>
+                       </td>
+                       </tr>
+                       </tbody>
+                   </table>
+               </div>
+               <div class="col-3">
+                   <div class="card text-center">
+                   <div class="card-body">
+                    <h3 class="text-muted">Total </h3>
+                    <h4 class="text-muted">$119.900</h4>
+                   <div class="text-center">
+                       <button class="btn btn-success" onClick={showModal}>comprar</button></div>
 
-                   
-                    <div class="p-2 d-flex gap-4">
-                        <div> 
-                            <img src="../img/882470265.jpg"  width="100rem" height="auto gap-2" alt="..."/>
-                        </div>
-
-                        <div>
-                            <h2 class="ml-1">Mochila KL</h2>
-                            <h6 class="text-muted">SKU 882470265</h6>
-                            <h6 class="text-muted">Negro</h6>
-                            <button type="button" class="btn btn-dark">Eliminar</button>
-                        </div>
-
-                        <div>
-                            <h2>$119.900</h2>
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <label class="input-group-text" for="inputGroupSelect01">Cantidad</label>
-                                </div>
-                                <select class="custom-select" id="inputGroupSelect01">
-
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                    <div class="card col-3">
-                    <div class="card-body">
-                    <h5 class="card-title">Tus compras</h5>
-                    <h6 class="card-subtitle mb-2 text-muted d-flex">productos:</h6><br/>
-                    <h6 class="card-subtitle mb-2 text-muted d-flex">Mochila KL <span class="col align-self-end">$119.900</span>
-                      </h6>
-                      <h6 class="card-subtitle mb-2 text-muted d-flex">Mochila KL <span class="col align-self-end">$119.900</span>
-                      </h6>
-                    <h3 class="text-muted d-flex">Total <span class="col align-self-end">$119.900</span></h3>
-                    <div class="d-flex flex-row-reverse"><button class="col align-self-end">comprar</button></div>
-
-                    </div>
-                </div>
-            
-            </div>
-            </div>
-          </div>
-        </div>
-        </div>
+                   </div>
+               </div>
+               </div>
+           
+           </div>
+           </div>
+         </div>
+       </div>
+       </div>
     );
 }
 export default Shopping;
